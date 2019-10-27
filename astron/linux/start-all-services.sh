ee352ecb0fd58c9a727876cbfde16ad30603eb96
nate97 <nathanielfuhr@gmail.com>
2019-10-27 00:30:00 -0400

    Add pause to start all services

--- astron/linux/start-all-services.sh
+++ astron/linux/start-all-services.sh
@@ -5,3 +5,5 @@ tmux new-session -d -s astron 'bash start-astron-cluster.sh'
 tmux new-session -d -s uberdog 'yes "" | bash start-uberdog-server.sh'
 
 tmux new-session -d -s ai 'yes "" | bash start-ai-server.sh'
+
+read -p "Press [Enter] key to exit..."
