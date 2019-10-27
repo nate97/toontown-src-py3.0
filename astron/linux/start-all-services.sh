7f7b0cc2a70ba5262667b63823eed2053d7b1f68
nate97 <nathanielfuhr@gmail.com>
2019-10-27 18:09:38 -0400

    fix

--- astron/linux/start-all-services.sh
+++ astron/linux/start-all-services.sh
@@ -4,8 +4,4 @@ tmux new-session -d -s astron 'bash start-astron-cluster.sh'
 tmux new-session -d -s uberdog 'yes "" | bash start-uberdog-server.sh'
 tmux new-session -d -s ai 'yes "" | bash start-ai-server.sh'
 
-echo "Game server has successfully started..."
-
-OUTPUT="$(hostname -I)" && echo "Server IP: ${OUTPUT}  Service port: 7199"
-
 read -p "Press [Enter] key to exit..."
